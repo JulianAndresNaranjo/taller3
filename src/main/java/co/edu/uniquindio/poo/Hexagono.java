e033e8cab171a56a1270ea185965ca67a74158ea
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 10:06:33 -0500

    Update Hexagono.java

--- src/main/java/co/edu/uniquindio/poo/Hexagono.java
+++ src/main/java/co/edu/uniquindio/poo/Hexagono.java
@@ -1,67 +1,28 @@
 package co.edu.uniquindio.poo;
 
-public class Hexagono extends FiguraRegular implements IFiguraGeometrica {
-    private double longitudLado;
+public class Hexagono  extends FiguraRegular implements IFiguraGeometrica,IFiguraRegular{
 
-    //CONSTRUCTOR
-    public Hexagono(double lado, double longitudLado) {
+    public Hexagono(double lado) {
         super(lado);
-        this.lado = lado;
-        this.longitudLado=longitudLado;
     }
 
-    //Métodos utilizados
     @Override
     public double calcularArea() {
-        double areaHexagono= (3/2) * Math.pow(longitudLado, 2) * Math.sqrt(3);
-        System.out.println("El área del Hexágono es de: " + areaHexagono);
-        return areaHexagono;
+        return (3 * Math.sqrt(3) * Math.pow(lado, 2)) / 2.0;
     }
 
     @Override
     public double calcularPerimetro() {
-        double perimetroHexagono = 6*longitudLado;
-        System.out.println("El perímetro del Hexágono es de: " +perimetroHexagono);
-        return perimetroHexagono;
+        return 6 * lado;
+    }
+    
+     @Override
+    public String calcularAngulo() {
+        return "cada angulo de un hexamono regular es de 60 grados y en total es de 360 grados";
     }
 
     @Override
-    public double calcularSumaAngulosInteriores() {
-        double angulosInterioresHexagono= (lado - 2) * 180;
-        System.out.println("La suma de los ángulos interiores del Hexágono es de: " + angulosInterioresHexagono);
-        return angulosInterioresHexagono;
+    public double calcularLongitud() {
+         return lado / (2.0 * Math.tan(Math.PI / 6));
     }
-
-    @Override
-    public double calcularLongitudApotema() {
-        double longitudApotemaHexagono=(longitudLado / 2) * (1 / Math.tan(Math.PI/5));
-        System.out.println("La longitud del Apotema del Hexágono es de: " + longitudApotemaHexagono);
-        return longitudApotemaHexagono;
-    }
-
-    //DEMÁS MÉTODOS NO UTILIZADOS
-    @Override
-    public double calcularDiagonalCuadrados() {
-        return 0.0;
-    }
-
-    @Override
-    public double calcularDiametroCirculos() {
-        return 0.0;
-    }
-
-    @Override
-    public double calcularAlturaTriangulos() {
-        return 0.0;
-    }
-
-    //GETTERS Y SETTERS
-    public double getLongitudLado() {
-        return longitudLado;
-    }
-
-    public void setLongitudLado(double longitudLado) {
-        this.longitudLado = longitudLado;
-    }
-
 }
