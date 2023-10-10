d80135e35f6478fcb53edf4ee6c9227cf38c886a
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 15:05:00 -0500

    Update Cuadrado.java

--- src/main/java/co/edu/uniquindio/poo/Cuadrado.java
+++ src/main/java/co/edu/uniquindio/poo/Cuadrado.java
@@ -1,30 +1,28 @@
 package co.edu.uniquindio.poo;
 
 public class Cuadrado extends FiguraRegular implements IFiguraGeometrica,IFiguraRegular{
-
+    //constructor
     public Cuadrado(double lado) {
         super(lado);
     }
-
-    @Override
+    
+    //metodos
     public double calcularArea() {
         return lado*lado;
     }
 
-    @Override
     public double calcularPerimetro() {
         return lado*4;
     }
 
-    @Override
     public String calcularAngulo() {
-        return "cada angulo del cuadrado mide 90 grados asi dado un angulo completo de 360 grados";
+        return "90";
     }
 
-    @Override
     public double calcularLongitud() {
         return lado/2.0;
     }
+
     public double calcularDiagonal(){
         return lado * Math.sqrt(2);
     }
