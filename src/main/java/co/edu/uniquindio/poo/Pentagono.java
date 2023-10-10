3dab4d4834ad3eb182af050ef2dbd36aa7f31a08
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 15:07:46 -0500

    Update Pentagono.java

--- src/main/java/co/edu/uniquindio/poo/Pentagono.java
+++ src/main/java/co/edu/uniquindio/poo/Pentagono.java
@@ -1,39 +1,29 @@
 package co.edu.uniquindio.poo;
 
 public class Pentagono  extends FiguraRegular implements IFiguraGeometrica,IFiguraRegular{
-
-    private double apotema;
-
+    //constructor
     public Pentagono(double lado) {
         super(lado);
-        this.apotema=apotema;
+
     }
 
-    @Override
+    //metodos
     public double calcularArea() {
-        return (5*lado * apotema) / 2.0;
+        return (5*lado * (lado / 2.0) / Math.tan(Math.toRadians(72) / 2.0)) / 2.0;
     }
 
-    @Override
     public double calcularPerimetro() {
         return 5 * lado;
     }
 
-    @Override
     public String calcularAngulo() {
-        return "cada angulo del pentagono mide 72 grados asi dado un angulo completo de 360 grados";
+        return "72";
     }
 
-    @Override
     public double calcularLongitud() {
-        return apotema=(lado / 2.0) / Math.tan(Math.toRadians(72) / 2.0);
-        
+        return (lado / 2.0) / Math.tan(Math.toRadians(72) / 2.0);
     }
-
-    public double getApotema() {
-        return apotema;
-    }
-
+}
     public void setApotema(double apotema) {
         this.apotema = apotema;
     }
