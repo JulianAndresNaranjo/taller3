052db8f82d8500895e0dcab1a61680dd8fad02b2
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 15:05:23 -0500

    Update FiguraRegular.java

--- src/main/java/co/edu/uniquindio/poo/FiguraRegular.java
+++ src/main/java/co/edu/uniquindio/poo/FiguraRegular.java
@@ -1,14 +1,16 @@
 package co.edu.uniquindio.poo;
 
 public class FiguraRegular {
-
+    //atributos
     protected double lado;
 
+    //constructor
     public FiguraRegular(double lado){
         this.lado=lado;
 
     }
 
+    //metodos
     public double getLado() {
         return lado;
 
