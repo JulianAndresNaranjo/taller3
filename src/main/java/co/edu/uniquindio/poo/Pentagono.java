54d3b68506d7c28acbbc08a49f56e226d9770c75
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 10:09:08 -0500

    Update Pentagono.java

--- src/main/java/co/edu/uniquindio/poo/Pentagono.java
+++ src/main/java/co/edu/uniquindio/poo/Pentagono.java
@@ -1,67 +1,40 @@
 package co.edu.uniquindio.poo;
 
-public class Pentagono extends FiguraRegular implements IFiguraGeometrica {
-    private double longitudLado;
+public class Pentagono  extends FiguraRegular implements IFiguraGeometrica,IFiguraRegular{
 
-    //CONSTRUCTOR
-    public Pentagono(double lado, double longitudLado) {
+    private double apotema;
+
+    public Pentagono(double lado) {
         super(lado);
-        this.lado = lado;
-        this.longitudLado=longitudLado;
+        this.apotema=apotema;
     }
 
-    //Métodos utilizados
     @Override
     public double calcularArea() {
-        double areaPentagono= (5/4) * Math.pow(longitudLado, 2)* (1 / Math.tan(Math.PI/5));
-        System.out.println("El área del Pentágono es de: " + areaPentagono);
-        return areaPentagono;
+        return (5*lado * apotema) / 2.0;
     }
 
     @Override
     public double calcularPerimetro() {
-        double perimetroPentagono = 5*longitudLado;
-        System.out.println("El perímetro del Pentágono es de: " +perimetroPentagono);
-        return perimetroPentagono;
+        return 5 * lado;
     }
 
     @Override
-    public double calcularSumaAngulosInteriores() {
-        double angulosInterioresPentagono= (lado - 2) * 180;
-        System.out.println("La suma de los ángulos interiores delPentágono es de: " + angulosInterioresPentagono);
-        return angulosInterioresPentagono;
+    public String calcularAngulo() {
+        return "cada angulo del pentagono mide 72 grados asi dado un angulo completo de 360 grados";
     }
 
     @Override
-    public double calcularLongitudApotema() {
-        double longitudApotemaPentagono=(longitudLado / 2) * (1 / Math.tan(Math.PI/5));
-        System.out.println("La longitud del Apotema del Pentágono es de: " + longitudApotemaPentagono);
-        return longitudApotemaPentagono;
+    public double calcularLongitud() {
+        return apotema=(lado / 2.0) / Math.tan(Math.toRadians(72) / 2.0);
+        
     }
 
-    //DEMÁS MÉTODOS NO UTILIZADOS
-    @Override
-    public double calcularDiagonalCuadrados() {
-        return 0.0;
+    public double getApotema() {
+        return apotema;
     }
 
-    @Override
-    public double calcularDiametroCirculos() {
-        return 0.0;
+    public void setApotema(double apotema) {
+        this.apotema = apotema;
     }
-
-    @Override
-    public double calcularAlturaTriangulos() {
-        return 0.0;
-    }
-
-    //GETTERS Y SETTERS
-    public double getLongitudLado() {
-        return longitudLado;
-    }
-
-    public void setLongitudLado(double longitudLado) {
-        this.longitudLado = longitudLado;
-    }
-
 }
