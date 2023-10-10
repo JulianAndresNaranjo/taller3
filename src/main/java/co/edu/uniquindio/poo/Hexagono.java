89c639d0b91fa81945d31ebcc2a9372804a15543
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 15:05:52 -0500

    Update Hexagono.java

--- src/main/java/co/edu/uniquindio/poo/Hexagono.java
+++ src/main/java/co/edu/uniquindio/poo/Hexagono.java
@@ -1,27 +1,24 @@
 package co.edu.uniquindio.poo;
 
 public class Hexagono  extends FiguraRegular implements IFiguraGeometrica,IFiguraRegular{
-
+    //constructor
     public Hexagono(double lado) {
         super(lado);
     }
 
-    @Override
+    //metodos
     public double calcularArea() {
         return (3 * Math.sqrt(3) * Math.pow(lado, 2)) / 2.0;
     }
 
-    @Override
     public double calcularPerimetro() {
         return 6 * lado;
     }
-    
-     @Override
+
     public String calcularAngulo() {
-        return "cada angulo de un hexamono regular es de 60 grados y en total es de 360 grados";
+        return "60";
     }
 
-    @Override
     public double calcularLongitud() {
          return lado / (2.0 * Math.tan(Math.PI / 6));
     }
