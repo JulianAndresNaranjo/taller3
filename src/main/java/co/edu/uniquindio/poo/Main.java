3a6f3f56c618f0b38f43e0f350e3b89812a86fdb
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 15:06:56 -0500

    Update Main.java

--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -7,81 +7,87 @@ public class Main {
      Scanner scanner = new Scanner(System.in);
 
         System.out.println("Seleccione una figura geometrica:");
-        System.out.println("1. Circulo\r\n" + //
-                "2. Triangulo\r\n" + //
-                "3. Cuadrado\r\n" + //
-                "4. Pentágono\r\n" + //
-                "5. Hexágono\r\n" + //
+        System.out.println("1. Circulo\r\n" + 
+                "2. Triangulo\r\n" + 
+                "3. Cuadrado\r\n" + 
+                "4. Pentágono\r\n" + 
+                "5. Hexágono\r\n" + 
                 "6. Octágono");
+        System.out.println("");
 
         int opcion = scanner.nextInt();
+        double lado;
 
-        IFiguraGeometrica ifiguraGeometrica = null;
+        System.out.println("");
 
         switch (opcion) {
             case 1:
-            System.out.print("---CIRCULO---" + "\n");
-                //LADO HACE COMO RADOIO.
-                double lado = 3;
-                ifiguraGeometrica= new Circulo(lado);
+                lado = 3;
+
+                Circulo Circulo = new Circulo(lado);
+                System.out.println("el area del circulo es de:"+Circulo.calcularArea());
+                System.out.println("el perimetrto del circulo es de:"+Circulo.calcularPerimetro());
+                System.out.println("el diametro del circulo es de:"+Circulo.calcularDiametro());
+                System.out.println("");
                 break;
 
-            case 2:
-                System.out.print("---TRIANGULO---" + "\n");
+             case 2:
                 lado = 3;
-                double longitudLado = 5;
-                double altura = 5;
-                double base= 5;
-                ifiguraGeometrica= new Triangulo (lado, base, altura,longitudLado);
+                Triangulo Triangulo = new Triangulo(lado);
+                System.out.println("el area del triangulo es de:"+Triangulo.calcularArea());
+                System.out.println("el perimetrto del triangulo es de:"+Triangulo.calcularPerimetro());
+                System.out.println("el angulo del triangulo regular es de "+Triangulo.calcularAngulo());
+                System.out.println("la longitud apotema del  triangulo es de:"+Triangulo.calcularLongitud());
+                System.out.println("la altura del triangulo es:"+Triangulo.calcularAltura());
+                System.out.println("");
                 break;
 
             case 3:
-                System.out.print("---CUADRADO---" + "\n");
                 lado = 4;
-                double ancho=5;
-                double largo=5;
-                longitudLado = 5;
-                ifiguraGeometrica= new Cuadrado(lado, ancho, largo, longitudLado);
+                Cuadrado Cuadrado = new Cuadrado(lado);
+                System.out.println("el area del cuadrado es de:"+Cuadrado.calcularArea());
+                System.out.println("el perimetrto del cuadrado es de:"+Cuadrado.calcularPerimetro());
+                System.out.println("el angulo del cuadrado es de:"+Cuadrado.calcularAngulo());
+                System.out.println("la longitud apotema del cuadrado es de:"+Cuadrado.calcularLongitud());
+                System.out.println("la diagonal del cuadrado es de:"+Cuadrado.calcularDiagonal());
+                System.out.println("");
                 break;
-            
+
             case 4:
-                System.out.print("---PENTÁGONO---" + "\n");
                 lado = 5;
-                longitudLado = 7;
-                ifiguraGeometrica= new Pentagono(lado,longitudLado);
+                Pentagono Pentagono= new Pentagono(lado);
+                System.out.println("el area del pentagono es de:"+Pentagono.calcularArea());
+                System.out.println("el perimetrto del pentagono es de:"+Pentagono.calcularPerimetro());
+                System.out.println("el angulo del pentagono es de:"+Pentagono.calcularAngulo());
+                System.out.println("la longitud apotema del pentagono es de:"+Pentagono.calcularLongitud());
+                System.out.println("");
                 break;
 
             case 5:
-                System.out.print("---HEXÁGONO---" + "\n");
                 lado = 6;
-                longitudLado = 8;
-                ifiguraGeometrica= new Hexagono(lado, longitudLado);
+                Hexagono Hexagono= new Hexagono(lado);
+                System.out.println("el area del hexagono es de:"+Hexagono.calcularArea());
+                System.out.println("el perimetrto del hexagono es de:"+Hexagono.calcularPerimetro());
+                System.out.println("el angulo del hexagono es de:"+Hexagono.calcularAngulo());
+                System.out.println("la longitud apotema del hexagono es de:"+Hexagono.calcularLongitud());
+                System.out.println("");
                 break;
             
             case 6:
-                System.out.print("---OCTÁGONO---" + "\n");
                 lado = 8;
-                longitudLado = 7;
-                ifiguraGeometrica= new Octagono(lado, longitudLado);
+                Octagono Octagono= new Octagono(lado);
+                System.out.println("el area del octagono es de:"+Octagono.calcularArea());
+                System.out.println("el perimetrto del octagono es de:"+Octagono.calcularPerimetro());
+                System.out.println("el angulo del octagono es de:"+Octagono.calcularAngulo());
+                System.out.println("la longitud apotema del octagono es de:"+Octagono.calcularLongitud());
+                System.out.println("");
                 break;
 
 
             default:
                 System.out.println("Opción no válida. Seleccione una opción válida.");
-        }
-
-        if (ifiguraGeometrica != null) {
-            ifiguraGeometrica.calcularArea();
-            ifiguraGeometrica.calcularPerimetro();
-            ifiguraGeometrica.calcularSumaAngulosInteriores();
-            ifiguraGeometrica.calcularLongitudApotema();
-            ifiguraGeometrica.calcularDiagonalCuadrados();
-            ifiguraGeometrica.calcularDiametroCirculos();
-            ifiguraGeometrica.calcularAlturaTriangulos();
-
-            System.out.println("Hemos Finalizado");
+                System.out.println("");
         }
     scanner.close();
-    
     }
 }
