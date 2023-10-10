ac458b2d2777b14362db914b6272a0d26ffa2fa4
JulianAndresNaranjo <142056387+JulianAndresNaranjo@users.noreply.github.com>
2023-10-10 15:07:21 -0500

    Update Octagono.java

--- src/main/java/co/edu/uniquindio/poo/Octagono.java
+++ src/main/java/co/edu/uniquindio/poo/Octagono.java
@@ -1,27 +1,24 @@
 package co.edu.uniquindio.poo;
 
 public class Octagono  extends FiguraRegular implements IFiguraGeometrica,IFiguraRegular{
-
+    //constructor
     public Octagono(double lado) {
         super(lado);
     }
 
-    @Override
+    //metodos
     public double calcularArea() {
         return 2 * Math.pow(lado, 2) * Math.tan(Math.PI / 8);
     }
 
-    @Override
     public double calcularPerimetro() {
         return 8 * lado;
     }
 
-    @Override
     public String calcularAngulo() {
-        return "cada angulo del octagono mide 45 grados asi dado un angulo completo de 360 grados";
+        return "45";
     }
 
-    @Override
     public double calcularLongitud() {
         return lado / (2.0 * Math.tan(Math.PI / 8));
     }
