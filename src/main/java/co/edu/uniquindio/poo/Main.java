876933500dc8d26b07fe4aeb84e2a827d4353c81
LauraMSanchez <131871346+LauraMSanchez@users.noreply.github.com>
2023-10-10 21:30:09 -0500

    Update Main.java

--- src/main/java/co/edu/uniquindio/poo/Main.java
+++ src/main/java/co/edu/uniquindio/poo/Main.java
@@ -26,61 +26,61 @@ public class Main {
 
                 lado = 3;
                 Circulo Circulo = new Circulo(lado);
-                System.out.println("el area del circulo es de:"+Circulo.calcularArea());
-                System.out.println("el perimetrto del circulo es de:"+Circulo.calcularPerimetro());
-                System.out.println("el diametro del circulo es de:"+Circulo.calcularDiametro());
+                System.out.println("El área del círculo es de: "+Circulo.calcularArea());
+                System.out.println("El perímetrto del círculo es de: "+Circulo.calcularPerimetro());
+                System.out.println("El diámetro del círculo es de: "+Circulo.calcularDiametro());
                 System.out.println("");
                 break;
 
                 case 2:
                 lado = 3;
                 Triangulo Triangulo = new Triangulo(lado);
-                System.out.println("el area del triangulo es de:"+Triangulo.calcularArea());
-                System.out.println("el perimetrto del triangulo es de:"+Triangulo.calcularPerimetro());
-                System.out.println("el angulo del triangulo regular es de "+Triangulo.calcularAngulo());
-                System.out.println("la longitud apotema del  triangulo es de:"+Triangulo.calcularLongitud());
-                System.out.println("la altura del triangulo es:"+Triangulo.calcularAltura());
+                System.out.println("El área del triángulo es de: "+Triangulo.calcularArea());
+                System.out.println("El perímetrto del triángulo es de: "+Triangulo.calcularPerimetro());
+                System.out.println("El ángulo del triángulo regular es de "+Triangulo.calcularAngulo());
+                System.out.println("La longitud apotema del triángulo es de: "+Triangulo.calcularLongitud());
+                System.out.println("La altura del triángulo es: "+Triangulo.calcularAltura());
                 System.out.println("");
                 break;
                 
                 case 3:
                 lado = 4;
                 Cuadrado Cuadrado = new Cuadrado(lado);
-                System.out.println("el area del cuadrado es de:"+Cuadrado.calcularArea());
-                System.out.println("el perimetrto del cuadrado es de:"+Cuadrado.calcularPerimetro());
-                System.out.println("el angulo del cuadrado es de:"+Cuadrado.calcularAngulo());
-                System.out.println("la longitud apotema del cuadrado es de:"+Cuadrado.calcularLongitud());
-                System.out.println("la diagonal del cuadrado es de:"+Cuadrado.calcularDiagonal());
+                System.out.println("El área del cuadrado es de: "+Cuadrado.calcularArea());
+                System.out.println("El perímetrto del cuadrado es de: "+Cuadrado.calcularPerimetro());
+                System.out.println("El ángulo del cuadrado es de: "+Cuadrado.calcularAngulo());
+                System.out.println("La longitud apotema del cuadrado es de: "+Cuadrado.calcularLongitud());
+                System.out.println("La diagonal del cuadrado es de: "+Cuadrado.calcularDiagonal());
                 System.out.println("");
                 break;
 
                 case 4:
                 lado = 5;
                 Pentagono Pentagono= new Pentagono(lado);
-                System.out.println("el area del pentagono es de:"+Pentagono.calcularArea());
-                System.out.println("el perimetrto del pentagono es de:"+Pentagono.calcularPerimetro());
-                System.out.println("el angulo del pentagono es de:"+Pentagono.calcularAngulo());
-                System.out.println("la longitud apotema del pentagono es de:"+Pentagono.calcularLongitud());
+                System.out.println("El area del pentágono es de: "+Pentagono.calcularArea());
+                System.out.println("El perimetrto del pentágono es de: "+Pentagono.calcularPerimetro());
+                System.out.println("El angulo del pentágono es de: "+Pentagono.calcularAngulo());
+                System.out.println("La longitud apotema del pentágono es de: "+Pentagono.calcularLongitud());
                 System.out.println("");
                 break;
 
                 case 5:
                 lado = 6;
                 Hexagono Hexagono= new Hexagono(lado);
-                System.out.println("el area del hexagono es de:"+Hexagono.calcularArea());
-                System.out.println("el perimetrto del hexagono es de:"+Hexagono.calcularPerimetro());
-                System.out.println("el angulo del hexagono es de:"+Hexagono.calcularAngulo());
-                System.out.println("la longitud apotema del hexagono es de:"+Hexagono.calcularLongitud());
+                System.out.println("El área del hexágono es de:"+Hexagono.calcularArea());
+                System.out.println("El perímetrto del hexágono es de:"+Hexagono.calcularPerimetro());
+                System.out.println("El ángulo del hexágono es de:"+Hexagono.calcularAngulo());
+                System.out.println("La longitud apotema del hexágono es de:"+Hexagono.calcularLongitud());
                 System.out.println("");
                 break;
 
                 case 6:
                 lado = 8;
                 Octagono Octagono= new Octagono(lado);
-                System.out.println("el area del octagono es de:"+Octagono.calcularArea());
-                System.out.println("el perimetrto del octagono es de:"+Octagono.calcularPerimetro());
-                System.out.println("el angulo del octagono es de:"+Octagono.calcularAngulo());
-                System.out.println("la longitud apotema del octagono es de:"+Octagono.calcularLongitud());
+                System.out.println("El area del octágono es de: "+Octagono.calcularArea());
+                System.out.println("El perimetrto del octágono es de: "+Octagono.calcularPerimetro());
+                System.out.println("El angulo del octágono es de: "+Octagono.calcularAngulo());
+                System.out.println("La longitud apotema del octágono es de: "+Octagono.calcularLongitud());
                 System.out.println("");
                 break;
 
